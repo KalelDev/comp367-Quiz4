06002e3a70965a82d44148baa0da30d0670de40a
KalelDev <akhalee1@my.centennialcollege.ca>
2024-04-13 17:01:44 -0400

    Add database access

---
+++ Quiz4/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Quiz4.Models;
+
+namespace Quiz4.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
